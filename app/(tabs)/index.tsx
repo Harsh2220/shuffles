68554b4e4945cf5d18080ad4fe5f9f5210457ef8
Harsh <harshgajjar145@gmail.com>
2024-03-10 21:58:27 +0530

    fix: generatewallet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,20 @@
+import Button from "@/src/components/UI/Button";
 import React from "react";
 import { Text } from "react-native";
+import * as Bip39 from "bip39";
 
 export default function Home() {
-  return <Text>Hello</Text>;
+  return (
+    <Button
+      onPress={() => {
+        try {
+          console.log(Bip39.generateMnemonic());
+        } catch (error) {
+          console.log(error);
+        }
+      }}
+    >
+      Hello
+    </Button>
+  );
 }
