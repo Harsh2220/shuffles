586a56c74fdf21007eef1756f7440180c898373f
Harsh <harshgajjar145@gmail.com>
2024-03-19 16:25:48 +0530

    chore: bottomtab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,14 @@
+import BarIcon from "@/src/assets/Icons/Bar";
+import BarOutline from "@/src/assets/Icons/BarOutline";
+import Clock from "@/src/assets/Icons/Clock";
+import ClockOutline from "@/src/assets/Icons/ClockOutline";
+import DCAIcon from "@/src/assets/Icons/DCAIcon";
+import DCAOutline from "@/src/assets/Icons/DCAOutline";
+import ExchangeIcon from "@/src/assets/Icons/ExchangeIcon";
+import ExchangeOutline from "@/src/assets/Icons/ExchangeOutline";
+import Wallet from "@/src/assets/Icons/Wallet";
+import WalletOutline from "@/src/assets/Icons/WalletOutline";
+import { black, white } from "@/src/constants/color";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
 import React from "react";
@@ -12,22 +23,73 @@ function TabBarIcon(props: {
 export default function TabLayout() {
   return (
     <Tabs
+      initialRouteName="limit"
       screenOptions={{
         tabBarActiveTintColor: "red",
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="dca"
         options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          headerTitle: "DCA",
+          title: "",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <DCAIcon width={26} height={26} color={black[700]} />
+            ) : (
+              <DCAOutline width={26} height={26} color={white[200]} />
+            ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="limit"
         options={{
-          title: "swap",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerTitle: "Limit",
+          title: "",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <BarIcon width={26} height={26} color={black[700]} />
+            ) : (
+              <BarOutline width={26} height={26} color={white[200]} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+          title: "",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Wallet width={26} height={26} color={black[700]} />
+            ) : (
+              <WalletOutline width={26} height={26} color={white[200]} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="bridge"
+        options={{
+          title: "",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <ExchangeIcon width={26} height={26} color={black[700]} />
+            ) : (
+              <ExchangeOutline width={26} height={26} color={white[200]} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          headerTitle: "Activity",
+          title: "",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Clock width={26} height={26} color={black[700]} />
+            ) : (
+              <ClockOutline width={26} height={26} color={white[200]} />
+            ),
         }}
       />
     </Tabs>
