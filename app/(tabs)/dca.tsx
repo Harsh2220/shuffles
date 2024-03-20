a453597a8c249b671065dc7ebad53be4593f9090
Harsh <harshgajjar145@gmail.com>
2024-03-20 23:57:34 +0530

    fea: box

--- app/(tabs)/dca.tsx
+++ app/(tabs)/dca.tsx
@@ -1,6 +1,118 @@
+import Button from "@/src/components/UI/Button";
+import Container from "@/src/components/UI/Container";
+import { Heading } from "@/src/components/UI/Heading";
+import { Paragraph } from "@/src/components/UI/Paragraph";
+import { white } from "@/src/constants/color";
+import { Image } from "expo-image";
 import React from "react";
-import { View } from "react-native";
+import { TextInput, View } from "react-native";
 
 export default function Dca() {
-  return <View></View>;
+  return (
+    <Container>
+      <View
+        style={{
+          flex: 1,
+          padding: 16,
+        }}
+      >
+        <Paragraph
+          style={{
+            fontSize: 14,
+            fontWeight: "500",
+            color: white[200],
+          }}
+        >
+          I want to allocate
+        </Paragraph>
+        <View
+          style={{
+            borderRadius: 32,
+            marginTop: 8,
+            borderWidth: 1,
+            borderColor: white[500],
+            backgroundColor: "#F1F3F4",
+            padding: 8,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: 10,
+              backgroundColor: white[800],
+              borderWidth: 1,
+              borderColor: white[500],
+              borderRadius: 100,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+              }}
+            >
+              <Image
+                source={require("../../src/assets/images/solana.png")}
+                style={{
+                  width: 36,
+                  height: 36,
+                  borderRadius: 50,
+                }}
+                contentFit="cover"
+              />
+              <View>
+                <Heading
+                  style={{
+                    fontSize: 16,
+                    fontWeight: "600",
+                  }}
+                >
+                  Jupiter
+                </Heading>
+                <Paragraph
+                  style={{
+                    fontSize: 12,
+                    fontWeight: "500",
+                    color: white[100],
+                  }}
+                >
+                  1000 JUP
+                </Paragraph>
+              </View>
+            </View>
+            <Button
+              onPress={() => {}}
+              size="small"
+              color="black"
+              style={{
+                paddingHorizontal: 16,
+                paddingVertical: 8,
+              }}
+            >
+              Use Max
+            </Button>
+          </View>
+          <View
+            style={{
+              alignItems: "center",
+              marginVertical: 24,
+            }}
+          >
+            <TextInput
+              style={{
+                fontSize: 58,
+                fontWeight: "600",
+                fontFamily: "SF_Semibold",
+              }}
+              placeholderTextColor={"#BEBFC3"}
+              placeholder="00"
+            />
+          </View>
+        </View>
+      </View>
+    </Container>
+  );
 }
